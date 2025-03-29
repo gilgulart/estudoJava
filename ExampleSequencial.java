8b2723e29afc4c67e9746269217eca356822340f
gilgulart <gilbertogulart70@gmail.com>
2025-03-29 09:58:26 -0300

    Um algoritmo que avalia se você pode sair de casa baseando-se nas condições climáticas

---
+++ ExampleSequencial.java
@@ -0,0 +1,8 @@
+public class ExampleSequencial {
+    public static void main (String[] args) {
+        int producta = 10;
+        int productb = 20;
+        int sum = producta + productb;
+        System.out.println("A soma de " + producta + " e " + productb + " é: " + sum);
+    }
+}
