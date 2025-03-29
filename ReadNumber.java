d2ee51903e74f18e6fca82319315b167af24c984
gilgulart <gilbertogulart70@gmail.com>
2025-03-29 18:35:13 -0300

    lista de exercicios

---
+++ ReadNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReadNumber {
+    public static void main(String[] args){
+        int numberOne, numberTwo;
+        Scanner enter;
+        enter = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        numberOne = enter.nextInt();
+        System.out.print("Digite o segundo número: ");
+        numberTwo = enter.nextInt();
+        if (numberOne > numberTwo){
+            System.out.print("O número " + numberOne + " é maior que o número " + numberTwo);
+        } else {
+            System.out.print("O número " + numberTwo + " é maior que o número " + numberOne );
+        }
+    }
+}
