ff5677be1abe6088c2611071fc7df22f723880d1
gilgulart <gilbertogulart70@gmail.com>
2025-03-29 18:50:57 -0300

    lista de exercicios

---
+++ Multiple.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int allNumber, fiveMultiples;
+        Scanner enter;
+        enter = new Scanner(System.in);
+        System.out.print("Digite seu número: ");
+        allNumber = enter.nextInt();
+        fiveMultiples = allNumber % 5;
+        if (fiveMultiples == 0) {
+            System.out.print("Seu número " + allNumber + " é um multiplo de 5");
+        } else {
+            System.out.print("Seu número " + allNumber + " não é um multiplo de 5");
+        }
+    }
+}
